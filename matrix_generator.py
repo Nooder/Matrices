115a8fae04978cd0fe9f3aab003083a263073ddb
Nader <nbeyzaei@gmail.com>
2019-02-01 19:26:15 -0800

    Fixed Matrix Addition for negative entries

--- matrix_generator.py
+++ matrix_generator.py
@@ -2,5 +2,5 @@ from random import randint
 
 def generate_matrix(num_rows, num_cols):
     # Create num_rows x num_cols matrix and fill it in with random number from 0-99
-    matrix = [[randint(0,99) for c in range(int(num_cols))] for r in range(int(num_rows))]
+    matrix = [[randint(-99,99) for c in range(int(num_cols))] for r in range(int(num_rows))]
     return matrix
--- matrix_printer.py
+++ matrix_printer.py
@@ -7,18 +7,20 @@ def print_matrix(matrix):
 
     # Get largest and smallest members to calculate cell printing width
     largest_width = len(str(largest_matrix_member(matrix)))
-    top_bottom_border_width = num_cols * (largest_width + 2) + (num_cols + 1)
+    top_bottom_border_width = num_cols * (largest_width + 2) + (num_cols * 2 + 1)
 
     # Print top boundary of matrix
-    # Formula: Number of columns * (Longest cell member length + 2) + (Number of columns + 1)
+    # Formula: Number of columns * (Longest cell member length + 2) + (Number of columns * 2 + 1)
+    # (Number of columns * 2 + 1) accounts for negative entires, otherwise (Number of columns + 1) can be used for non-negative matrices
     print("-" * top_bottom_border_width)
     for r in range(int(num_rows)):
         # Print left boundary of matrix for this row
         print("|", end="")
         for c in range(int(num_cols)):
             # Print each column member of the row
-            # Formula for left-hand spacing: longest cell member length - current cell member length + 1
-            right_hand_spacing = largest_width - len(str(matrix[r][c])) + 1
+            # Formula for left-hand spacing: longest cell member length - current cell member length + 2
+            # +2 accounts for negative entries, otherwise +1 can be used for non-negative matrices
+            right_hand_spacing = largest_width - len(str(matrix[r][c])) + 2
             print(" " * right_hand_spacing + str(matrix[r][c]) + " |" , end = "")
         # Print a new line for the next row's columns
         print("")
